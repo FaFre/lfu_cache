865f7294dffb1b3887a334805e345b6483f80641
Fabian Freund <github@fabi.online>
2021-12-19 01:05:45 +0100

    initial package

---
+++ example/lfu_cache_example.dart
@@ -0,0 +1,6 @@
+import 'package:lfu_cache/lfu_cache.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/lfu_cache.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lfu_cache;
+
+export 'src/lfu_cache_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
